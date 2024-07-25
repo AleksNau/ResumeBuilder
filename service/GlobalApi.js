8de3afda433a72e47573f638c3d72156ce83e88b
AleksNau <workciti@yandex.ru>
2024-07-25 17:00:04 +0300

    api

--- service/GlobalApi.js
+++ service/GlobalApi.js
@@ -1,11 +1,17 @@
-const axios = require('axios').default;
+import axios from "axios";
 
 const API_KEY = import.meta.env.VITE_STRAPI_API_KEY;
 
 const axiosClient = axios.create({
-    baseURL:"http://localhost:1337/api/",
+    baseURL:"http://localhost:1337/",
     headers: {
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${API_KEY}`
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${API_KEY}`,
     }
 })
+
+const createNewResume = (data) => axiosClient.post('user-resumes',data)
+
+export default {
+    createNewResume,
+}
--- src/dashboard/components/AddResume.jsx
+++ src/dashboard/components/AddResume.jsx
@@ -11,15 +11,28 @@ import { Input } from "@/components/ui/input"
 import { Button } from "@/components/ui/button";
 import { useState } from "react";
 import { v4 as uuidv4 } from 'uuid';
+import GlobalApi from "../../../service/GlobalApi";
+import {useUser} from '@clerk/clerk-react'
 
 
 const AddResume = () => {
   const [openDialog, setOpenDialog] = useState(false);
   const [title, setTitle] = useState("");
+  const {user} = useUser()
 
   const onCreate = () => {
-    const id = uuidv4(); 
-    console.log(title, id)
+    console.log(user)
+    let uuid = uuidv4();
+    const data = {
+      data: {
+        title:title,
+        resumeid:uuid,
+        userEmail:user?.primaryEmailAddress?.emailAddress,
+        userName:user?.fullName
+      }
+    }
+
+    GlobalApi.createNewResume(data).then(res => console.log(res))
   }
   return (
     <div>
